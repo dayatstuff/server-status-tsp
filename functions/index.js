3c5eb8139497d27ed32ba12fb9bc0189a7d6fa8d
dayatstuff <petruksaja@gmail.com>
2018-10-30 21:39:26 +0700

    add cors buat functions

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,16 @@
 const functions = require('firebase-functions');
+
 const express = require('express') ;
+const cors = require('cors');
+
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 const app = express(); 
+
+app.use(cors({origin: true}));
 
 app.get('/status', (request, response) => {
     let sekarang = Date.now() ;
@@ -142,6 +148,77 @@ app.get('/status-cached', (resuest, response) => {
 
 
     response.send( json ) ;
+});
+
+
+app.get('/status.json', (resuest, response) => {
+  response.set('Cache-Control', 'public, max-age=30, s-maxage=60') ;
+
+
+  let sekarang = Date.now() ;
+  let hours = (mulai) => Math.round(Math.abs(sekarang - mulai) / 36e5 ) ;
+  let serverStatus = () => (Math.floor(Math.random() * 5) > 0 ? "UP" : "DOWN") ;
+  
+
+  let json = [{
+      "id": 1,
+      "alias": "Anderson",
+      "location": "Semarang",
+      "uptime": hours(Date.parse('May 12, 2014')),
+      "status": serverStatus()
+    },
+    {
+      "id": 2,
+      "alias": "Nabila",
+      "location": "Semarang",
+      "uptime": hours( Date.parse('June 12, 2012') ),
+      "status": serverStatus()
+    },
+    {
+      "id": 3,
+      "alias": "Tungslup",
+      "location": "Semarang",
+      "uptime": hours(Date.parse('May 10, 2014')),
+      "status": serverStatus()
+    },
+    {
+      "id": 4,
+      "alias": "LSM",
+      "location": "Singapore",
+      "uptime": hours( Date.parse('March 12, 2015') ),
+      "status": serverStatus()
+    },
+    {
+      "id": 5,
+      "alias": "Growell",
+      "location": "Singapore",
+      "uptime": hours(Date.parse('June 3, 2014')),
+      "status": serverStatus()
+    },
+    {
+      "id": 6,
+      "alias": "Ibra",
+      "location": "Semarang",
+      "uptime": hours( Date.parse('September 12, 2015') ),
+      "status": serverStatus()
+    },
+    {
+      "id": 7,
+      "alias": "Wongfeihung",
+      "location": "Ningxia",
+      "uptime": hours(Date.parse('August 20, 2016')),
+      "status": serverStatus()
+    },
+    {
+      "id": 8,
+      "alias": "Budak",
+      "location": "Semarang",
+      "uptime": hours( Date.parse('January 10, 2016') ),
+      "status": serverStatus()
+    }];
+
+
+  response.send( json ) ;
 });
 
 
